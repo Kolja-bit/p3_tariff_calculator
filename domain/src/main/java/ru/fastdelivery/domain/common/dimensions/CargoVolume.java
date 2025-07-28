00f48c8fa2c3bbd73af94f34a3583c7a376c8fb5
Wasilii Martynov <nishilov@yandex.ru>
2025-07-28 17:47:05 +0300

    attempt2

--- domain/src/main/java/ru/fastdelivery/domain/common/dimensions/CargoVolume.java
+++ domain/src/main/java/ru/fastdelivery/domain/common/dimensions/CargoVolume.java
@@ -1,50 +1,7 @@
 package ru.fastdelivery.domain.common.dimensions;
 
-import ru.fastdelivery.domain.common.weight.Weight;
-
 import java.math.BigDecimal;
-import java.math.BigInteger;
 
-public class CargoVolume {
-    private static final BigInteger SIZE_MAX=new BigInteger(String.valueOf(1500));
-    private final BigInteger length;
-    private final BigInteger width;
-    private final BigInteger height;
-    public CargoVolume(BigInteger length,BigInteger width,BigInteger height){
-        this.length=length;
-        this.width=width;
-        this.height=height;
-    }
-
-    public BigInteger getVolumeOfOnePackage(){
-        if (sizeСheck(length) || sizeСheck(width) || sizeСheck(height)){
-            throw new IllegalArgumentException("Size is out of range!");
-        }
-        BigDecimal resultLength=roundingOfPackagingDimensions(length);
-        BigDecimal resultWidth=roundingOfPackagingDimensions(width);
-        BigDecimal resultHeight=roundingOfPackagingDimensions(height);
-        BigInteger volumeOfOnePackage = calculationOfCargoPackagingVolume(resultLength,
-                resultWidth, resultHeight).toBigInteger();
-        return volumeOfOnePackage;
-    }
-    public BigDecimal calculationOfCargoPackagingVolume(BigDecimal length,
-                                                        BigDecimal width,BigDecimal height){
-        BigDecimal volume = length.multiply(width).multiply(height)
-                .divide(new BigDecimal(1000000000));
-        return volume;
-    }
-    public BigDecimal roundingOfPackagingDimensions(BigInteger sizeParameter){
-        BigDecimal resultParameter=new BigDecimal(sizeParameter)
-                .add(new BigDecimal(50))
-                .add(new BigDecimal(sizeParameter)
-                        .remainder(new BigDecimal(50)).multiply(new BigDecimal(-1)));
-        return resultParameter;
-    }
-    public boolean sizeСheck(BigInteger size){
-        boolean control=false;
-        if ( BigInteger.ZERO.compareTo(size)<=0 || SIZE_MAX.compareTo(size)<=0){
-            control = true;
-        }
-        return control;
-    }
+public interface CargoVolume {
+    BigDecimal getVolumeOfOnePackage();
 }
---
+++ domain/src/main/java/ru/fastdelivery/domain/common/dimensions/CargoVolumeImpl.java
@@ -0,0 +1,43 @@
+package ru.fastdelivery.domain.common.dimensions;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public record CargoVolumeImpl(BigInteger length,
+                              BigInteger width, BigInteger height) implements CargoVolume{
+    private static final BigInteger SIZE_MAX=new BigInteger(String.valueOf(1500));
+
+    public BigDecimal getVolumeOfOnePackage(){
+        if (sizeСheck(length) || sizeСheck(width) || sizeСheck(height)){
+            throw new IllegalArgumentException("Size is out of range!");
+        }
+        BigDecimal resultLength=roundingOfPackagingDimensions(length);
+        BigDecimal resultWidth=roundingOfPackagingDimensions(width);
+        BigDecimal resultHeight=roundingOfPackagingDimensions(height);
+        /*new TotalVolume(calculationOfCargoPackagingVolume(resultLength,
+                resultWidth, resultHeight));*/
+        BigDecimal volumeOfOnePackage = calculationOfCargoPackagingVolume(resultLength,
+                resultWidth, resultHeight);
+        return volumeOfOnePackage;
+    }
+    public BigDecimal calculationOfCargoPackagingVolume(BigDecimal length,
+                                                        BigDecimal width,BigDecimal height){
+        BigDecimal volume = length.multiply(width).multiply(height)
+                .divide(new BigDecimal(1000000000));
+        return volume;
+    }
+    public BigDecimal roundingOfPackagingDimensions(BigInteger sizeParameter){
+        BigDecimal resultParameter=new BigDecimal(sizeParameter)
+                .add(new BigDecimal(50))
+                .add(new BigDecimal(sizeParameter)
+                        .remainder(new BigDecimal(50)).multiply(new BigDecimal(-1)));
+        return resultParameter;
+    }
+    public boolean sizeСheck(BigInteger size){
+        boolean control=false;
+        if ( BigInteger.ZERO.compareTo(size)<=0 || SIZE_MAX.compareTo(size)<=0){
+            control = true;
+        }
+        return control;
+    }
+}
